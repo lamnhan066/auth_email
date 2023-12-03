5c9d17acc25cac59635edf9ea8abc4f31f1692fc
lamnhan066 <hello@lamnhan.dev>
2023-12-03 16:24:28 +0700

    Improve config.php comments

--- server/php/src/config.php
+++ server/php/src/config.php
@@ -1,19 +1,29 @@
 <?php
 
-// Version: 0.0.3
+// Version: 0.0.4
 
+// Set the SMTP server to send through
 $HOST = '';
+// SMTP username
 $USER_NAME = '';
+// SMTP password
 $PASSWORD = '';
+// TCP port to connect to
 $PORT = 587;
+// Send from
 $SEND_FROM = $USER_NAME;
 
+// Default subject of Email
 $DEFAULT_SUBJECT = 'Verify Email';
-$DEFAULT_BODY = 'Please use this OTP to verify your email for the <b>{appName}</b>, and do not share this code with anyone: <b>{otp}</b>';
+// Default body of Email
+$DEFAULT_BODY = 'Please use this OTP to verify your email for the <b>{appName}</b>, do not share this code to anyone: <b>{otp}</b>';
+// Default length of OTP
 $DEFAULT_OTP_LENGTH = 6;
 
+// Client key in SHA256 encryption
 $SERVER_SHA256_KEY = '';
 
+// Security of your applications.
 $ALLOWED_APPS = [
     // 'App Name' => [
     //	   'modifiedSubject' => false,
